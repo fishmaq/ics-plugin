7d4017c7ad6deb682e97f3205ab4b630d4c3508a
Martin Cerny <martin.cerny@itsv.at>
2025-07-22 01:31:19 +0200

    added changes for mapping recurring events

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
+import datetime
+from dateutil.relativedelta import relativedelta
 import json
+import subprocess
 
 import icalendar
-import datetime
 import pytz
-import subprocess
 
 # this script is based on https://github.com/klemensschindler/icalfilter/blob/master/icalfilter.py (18.07.2025)
 # REQUIREMENTS: pip install pytz icalendar && sudo apt install syncevolution
@@ -18,10 +19,13 @@ master_events_arr = []
 override_events_dict = {}
 
 # get the date of this week's monday and sunday, to only display events that are happening this week
-first_day_of_curr_week = (datetime.datetime.today() -
-                          datetime.timedelta(days=datetime.datetime.now().isocalendar().weekday - 1))
+today = datetime.datetime.today()
+first_day_of_curr_week = (
+        datetime.datetime(year=today.year, month=today.month, day=today.day, hour=0, minute=0, second=0) -
+        datetime.timedelta(days=datetime.datetime.now().isocalendar().weekday - 1))
 last_day_of_curr_week = (first_day_of_curr_week +
-                         datetime.timedelta(days=7))
+                         datetime.timedelta(days=6))
+print(first_day_of_curr_week)
 
 
 def sync_ics():
@@ -47,7 +51,7 @@ def handle_if_recurring_event(event_to_check):
         master_events_arr.append(event_to_check)
         return True
     elif 'RECURRENCE-ID' in event_to_check:
-        # if there is a recurrence-id, this means, that it an override event
+        # if there is a recurrence-id, this means, that it's an override event
         if event_to_check['UID'] not in override_events_dict:
             override_events_dict[(event_to_check['UID'])] = []
         override_events_dict[(event_to_check['UID'])].append(event_to_check)
@@ -56,21 +60,33 @@ def handle_if_recurring_event(event_to_check):
         return False
 
 
-def handle_regular_if_active(event_to_check):
-    start_date = event_to_check['dtstart'].dt
+# check if the start date is relevant in this week
+def active_start_date(start_date):
     active = False
+
     if type(start_date) == datetime.datetime:
+        if start_date.tzinfo is None or start_date.tzinfo.utcoffset(start_date) is None:
+            start_date = utc.localize(start_date)
         # check if event is in the current week
         active = utc.localize(first_day_of_curr_week) <= start_date <= utc.localize(
             last_day_of_curr_week)
     elif type(start_date) == datetime.date:
         # check if event is in the current week
-        active = datetime.date(first_day_of_curr_week.year, first_day_of_curr_week.month,
-                               first_day_of_curr_week.day) <= start_date <= datetime.date(
-            last_day_of_curr_week.year, last_day_of_curr_week.month,
-            last_day_of_curr_week.day)
+        active = (datetime.date(first_day_of_curr_week.year, first_day_of_curr_week.month,
+                                first_day_of_curr_week.day) <= start_date
+                  <= datetime.date(
+                    last_day_of_curr_week.year, last_day_of_curr_week.month,
+                    last_day_of_curr_week.day))
+    else:
+        print('')
 
-    if active:
+    return active
+
+
+def handle_regular_if_active(event_to_check):
+    start_date = event_to_check['dtstart'].dt
+
+    if active_start_date(start_date):
         regular_events_arr.append(event_to_check)
 
 
@@ -98,29 +114,94 @@ def map_event(event):
     # if item['description'] is not None:
     #     data['description'] = item['description']
     # data['attendee'] = item['attendee']
-    # TODO: mark events as recurring and how often
-    # TODO: support all day events
-
-    # print(json.dumps(out_dict))
-    # print('-----------------------------------')
-    return out_dict
-
-
-def map_recurring_event(event, override_event_arr):
-    out_dict = map_event(event)
-
-    out_dict['overrides'] = []
-
-    # FIXME: map all recurring events to regular events including overrides
-    for event in override_event_arr:
-        override_event_dict = map_event(event)
-        out_dict['overrides'].append(override_event_dict)
 
     return out_dict
+
+
+# TODO: add better comments
+def map_recurring_event(master_event, override_event_arr):
+    out_dict_arr = []
+    exclude_date_arr = []
+
+    # sort out irrelevant override events
+    override_event_arr = [event for event in override_event_arr if active_start_date(event['dtstart'].dt)]
+    if 'EXDATE' in master_event:
+        if type(master_event['exdate']) is list:
+            for exclude_date_entry in master_event['exdate']:
+                for date in exclude_date_entry.dts:
+                    exclude_date_arr.append(date)
+        else:
+            for date in master_event['exdate'].dts:
+                exclude_date_arr.append(date)
+
+    if type(master_event['dtstart'].dt) == datetime.datetime:
+        max_date = utc.localize(
+            last_day_of_curr_week)
+        master_event_start = master_event['dtstart'].dt
+        master_event_end = master_event['dtend'].dt
+        date = master_event_start
+
+        while date <= max_date:
+            if date not in exclude_date_arr and by_day(master_event['rrule']['byday'], date):
+                # if there is an override event for the day then use the override event
+                filtered_override_event_arr = [event for event in override_event_arr if
+                                               event['dtstart'].dt == date]
+                if not len(filtered_override_event_arr) > 0:
+                    # generate object
+                    out_dict_arr.append({
+                        'title': master_event['summary'],
+                        # use date from loop and time from master_event
+                        'start': datetime.datetime(date.year, date.month, date.day,
+                                                   master_event_start.hour,
+                                                   master_event_start.minute),
+                        'end': datetime.datetime(date.year, date.month, date.day,
+                                                 master_event_end.hour, master_event_end.minute)}
+                    )
+                else:
+                    # use override event
+                    override_event = filtered_override_event_arr[0]
+                    out_dict_arr.append({
+                        'title': override_event['summary'],
+                        # use date from loop and time from master_event
+                        'start': override_event['dtstart'].dt,
+                        'end': override_event['dtstart'].dt}
+                    )
+
+            interval = 1
+            if 'INTERVAL' in master_event['rrule']:
+                interval = master_event['rrule']['interval'][0]
+
+            i = 0
+            while i < interval:
+                match master_event['rrule']['freq'][0]:
+                    case 'MONTHLY':
+                        date = date + relativedelta(months=1)
+                    case 'YEARLY':
+                        break
+                    case 'WEEKLY':
+                        # when the event has a by day attribute, each day of the week has to be checked for byday match
+                        if 'BYDAY' in master_event['rrule']:
+                            date = date + relativedelta(days=1)
+                        else:
+                            date = date + relativedelta(weeks=1)
+                    case 'DAILY':
+                        date = date + relativedelta(days=1)
+                i += 1
+    else:
+        # TODO:
+        print('date not supported yet')
+
+    return out_dict_arr
+
+
+weekday_arr = ['MO', 'TU', 'WE', 'TH', 'FR', 'SA', 'SU']
+
+
+def by_day(by_day_arr, date):
+    return weekday_arr[date.weekday()] in by_day_arr
 
 
 def master_event_active(event_to_check):
-    active = False
     start_date = event_to_check['dtstart'].dt
     until_date = None
 
@@ -154,12 +235,12 @@ def master_event_active(event_to_check):
     # check if master event hasn't already ended
     if until_date is not None:
         if type(until_date) == datetime.datetime:
-            active = active and until_date >= utc.localize(last_day_of_curr_week)
+            active = active and until_date >= utc.localize(first_day_of_curr_week)
         elif type(until_date) == datetime.date:
             # FIXME: weird intellij warning that i dont understand
             active = active and until_date >= datetime.date(
-                last_day_of_curr_week.year, last_day_of_curr_week.month,
-                last_day_of_curr_week.day)
+                first_day_of_curr_week.year, first_day_of_curr_week.month,
+                first_day_of_curr_week.day)
 
     return active
 
@@ -177,28 +258,32 @@ def main():
         event_arr = [item for item in cal.subcomponents if item.name == 'VEVENT']
 
         for event in event_arr:
-            handle_if_recurring_event(event)
-            handle_regular_if_active(event)
+            if not handle_if_recurring_event(event):
+                handle_regular_if_active(event)
 
         out_arr = []
 
         # put all regular events into out arr as they are already filtered
         for event in regular_events_arr:
             json_dict_regular = map_event(event)
-            out_arr.append(json.dumps(json_dict_regular))
+            out_arr.append(json_dict_regular)
 
-        # filter out all master events, that aren't in this week
+            # filter out all master events, that aren't in this week
 
-        for event in [master_event for master_event in master_events_arr if master_event_active(master_event)]:
+        for master_event in [master_event for master_event in master_events_arr if master_event_active(master_event)]:
             # TODO: add exceptiondate support (exdate)
             # if there are any override events for the master event, handle them too
-            if event['UID'] in override_events_dict:
-                json_dict_master = map_recurring_event(event, override_events_dict[event['UID']])
+            if master_event['UID'] in override_events_dict:
+                json_arr_master = map_recurring_event(master_event, override_events_dict[master_event['UID']])
             else:
                 # no override elements, means empty arr as parameter
-                json_dict_master = map_recurring_event(event, [])
+                json_arr_master = map_recurring_event(master_event, [])
 
-            out_arr.append(json.dumps(json_dict_master))
+            for generated_event in json_arr_master:
+                if active_start_date(generated_event['start']):
+                    generated_event['start'] = generated_event['start'].strftime('%d.%m.%Y %H:%M')
+                    generated_event['end'] = generated_event['end'].strftime('%d.%m.%Y %H:%M')
+                    out_arr.append(generated_event)
 
         # print events to console
         print(json.dumps(out_arr))
